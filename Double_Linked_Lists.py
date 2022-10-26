eb3b40749a6f6c002619291c9846ed5e773dc1ed
akashgreninja <akashuhulekal@gmail.com>
2022-10-26 18:07:47 +0530

    Added all functionality of DLL

--- Double_Linked_Lists.py
+++ Double_Linked_Lists.py
@@ -1,6 +1,5 @@
 
 
-
 class Node:
     def __init__(self, value=None):
         self.value = value
@@ -30,10 +29,8 @@ class DLL:
 
     def insert(self, value, location):
 
-
         if self.head == None:
-            print( "node does not exist")
-        
+            print("node does not exist")
 
         else:
             newnode = Node(value)
@@ -58,14 +55,11 @@ class DLL:
                     tempnode = tempnode.next
                     index += 1
 
-
-
                 # newnode.next = tempnode.next
                 # newnode.prev=tempnode
                 # # this line gets the prev of the next node
                 # newnode.prev.next = newnode
                 # tempnode.next=newnode
-
 
                 # my logic
 
@@ -75,22 +69,99 @@ class DLL:
                 tempnode.next = newnode
                 nextnode.prev = newnode
 
-              
+    def transversal(self):
+        if self.head == None:
+            print("head does not exist")
+        node = self.head
+        while node is not None:
+            print(node.value)
+            node = node.next
+
+    def reverse_transversal(self):
+        if self.head == None:
+            print("head does not exist")
+        node = self.tail
+        while node is not None:
+            print(node.value)
+            node = node.prev
+
+    def search(self, value):
+        if self.head == None:
+            print("head does not exist")
+        node = self.head
+        while node:
+            if node.value == value:
+                print(f"{node.value} found")
+
+            node = node.next
+
+    def delete(self, location):
+        if self.head == None:
+            print("node does not exist")
+
+        else:
+            if location == 0:
+                if self.tail == self.head:
+                    self.head = None
+                    self.tail = None
+
+                else:
+                    self.head = self.head.next
+                    self.head.prev = None
+
+            elif location == -1:
+                if self.tail == self.head:
+                    self.head = None
+                    self.tail = None
+
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = None
+
+            else:
+                tempnode = self.head
+                index = 0
+                while index < location-1:
+                    tempnode = tempnode.next
+                    index += 1
+
+                nextnode = tempnode.next
+                tempnode.next = nextnode.next
+                nextnode.next.prev = tempnode
+
+    def deleteall(self):
+
+        tempnode = self.head
+        while tempnode:
+            tempnode.prev=None
+            tempnode=tempnode.next
+        
+        self.head=None
+        self.tail=None
+        
+        print("done")
 
 
-
-
-           
 
 
 createdDDL = DLL()
 createdDDL.createDLL(3)
 
 
-
 print([i.value for i in createdDDL])
 createdDDL.insert(1, 0)
 createdDDL.insert(2, -1)
+createdDDL.insert(3, -1)
 createdDDL.insert(2, 1)
 
+print([i.value for i in createdDDL])
+
+createdDDL.transversal()
+createdDDL.reverse_transversal()
+createdDDL.search(2)
+
+createdDDL.delete(2)
+print([i.value for i in createdDDL])
+
+createdDDL.deleteall()
 print([i.value for i in createdDDL])
